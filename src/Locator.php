4d538663ac181e99d76c762007195f472ad810c4
Evgeniy Kuvshinov <smpl@itmywork.com>
2015-02-27 03:23:30 +0500

    Возможность логирования fixed #25

--- src/Locator.php
+++ src/Locator.php
@@ -1,10 +1,15 @@
 <?php
 namespace smpl\mydi;
 
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use smpl\mydi\container\Service;
 
-class Locator implements LocatorInterface
+class Locator implements LocatorInterface, LoggerAwareInterface
 {
+    use LoggerAwareTrait;
     private $containers = [];
     private $calls = [];
     /**
@@ -12,28 +17,43 @@ class Locator implements LocatorInterface
      */
     private $loaders = [];
 
-    public function __construct(array $loaders = [])
+    public function __construct(array $loaders = [], LoggerInterface $logger = null)
     {
+        $this->setLogger(is_null($logger) ? new NullLogger() : $logger);
         $this->setLoaders($loaders);
     }
 
     public function getLoader($name)
     {
+        $this->logger->debug('Locator getLoader to load container {name}', ['name' => $name]);
         $result = null;
         foreach ($this->loaders as $loader) {
             if ($loader->isLoadable($name)) {
                 $result = $loader;
             }
         }
+        if (is_null($result)) {
+            $this->logger->info('Loader not found to load {name}', ['name' => $name]);
+        } else {
+            $this->logger->info('Loader not found to load {name}', ['name' => $name, 'class' => get_class($result)]);
+        }
         return $result;
     }
 
     public function resolve($name)
     {
+        $this->logger->debug('Locator resolve {name}', ['name' => $name]);
         if (!$this->isExist($name)) {
             if (is_null($loader = $this->getLoader($name))) {
                 throw new \InvalidArgumentException(sprintf('Name is not defined, %s', $name));
             }
+            $this->logger->info(
+                'Locator resolve {name} with Loader {class}',
+                [
+                    'name' => $name,
+                    'class' => get_class($loader)
+                ]
+            );
             $this->add($name, $loader->load($name));
         }
         $result = $this->containers[$name];
@@ -44,14 +64,23 @@ class Locator implements LocatorInterface
         }
         array_push($this->calls, $name);
         if ($result instanceof ContainerInterface) {
+            $this->logger->info(
+                'Locator use ContainerInterface {class} to resolve {name}',
+                [
+                    'class' => get_class($result),
+                    'name' => $name
+                ]
+            );
             $result = $result->resolve();
         }
         array_pop($this->calls);
+        $this->logger->info('Locator container {name} is resolved', ['name' => $name]);
         return $result;
     }
 
     public function add($name, $value)
     {
+        $this->logger->debug('Locator add container {name}', ['name' => $name]);
         if ($this->isExist($name)) {
             throw new \InvalidArgumentException(sprintf('name is already exist, %s', $name));
         }
@@ -60,6 +89,7 @@ class Locator implements LocatorInterface
 
     public function delete($name)
     {
+        $this->logger->debug('Locator delete container {name}', ['name' => $name]);
         if (!$this->isExist($name)) {
             throw new \InvalidArgumentException(sprintf('name is not exist, %s', $name));
         }
@@ -68,10 +98,12 @@ class Locator implements LocatorInterface
 
     public function set($name, $value)
     {
+        $this->logger->debug('Locator set container {name}', ['name' => $name]);
         if (!is_string($name)) {
             throw new \InvalidArgumentException('name must be string');
         }
         if (is_callable($value)) {
+            $this->logger->info('Used default wrapper for callable element', ['name' => $name]);
             $value = new Service($value);
         }
         $this->containers[$name] = $value;
@@ -91,6 +123,7 @@ class Locator implements LocatorInterface
 
     public function isExist($name)
     {
+        $this->logger->debug('Locator isExist container {name}', ['name' => $name]);
         return array_key_exists($name, $this->containers);
     }
 
@@ -129,6 +162,7 @@ class Locator implements LocatorInterface
      */
     public function getLoaders()
     {
+        $this->logger->debug('Locator {method}', ['method' => __METHOD__]);
         return $this->loaders;
     }
 
@@ -138,10 +172,12 @@ class Locator implements LocatorInterface
      */
     public function setLoaders(array $loaders)
     {
+        $this->logger->debug('Locator {method}', ['method' => __METHOD__]);
         foreach ($loaders as $loader) {
             if (!($loader instanceof LoaderInterface)) {
                 throw new \InvalidArgumentException('Loaders must imlemenent \smpl\mydi\LoaderInterface');
             }
+            $this->logger->info('set loader {class}', ['class' => get_class($loader)]);
         }
         $this->loaders = $loaders;
     }
