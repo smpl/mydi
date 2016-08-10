430fcd9d352d14f1846db3f177b075886449bd92
Evgeniy Kuvshinov <smpl@itmywork.com>
2016-08-10 19:20:28 +0500

    Вытащил генерацию файлов из тестов в готовые файлы

--- tests/loader/IoCTest.php
+++ tests/loader/IoCTest.php
@@ -6,47 +6,6 @@ use smpl\mydi\container\IoC;
 
 class IoCTest extends \PHPUnit_Framework_TestCase
 {
-
-    public static function setUpBeforeClass()
-    {
-        parent::setUpBeforeClass();
-
-        $subDirTest = <<<'php'
-<?php
-return 15;
-php;
-        $test = <<<'php'
-<?php
-return 15;
-php;
-        $testOutput = <<<'php'
-<?php
-echo 'Magic';
-return 15;
-php;
-        $root = __DIR__ . DIRECTORY_SEPARATOR . 'tmp' . DIRECTORY_SEPARATOR;
-        mkdir($root);
-        file_put_contents($root . 'test.php', $test);
-        file_put_contents($root . 'testOutput.php', $testOutput);
-        mkdir($root . 'subDir');
-        file_put_contents(
-            $root . 'subDir' . DIRECTORY_SEPARATOR . 'test.php',
-            $subDirTest
-        );
-    }
-
-    public static function tearDownAfterClass()
-    {
-        parent::tearDownAfterClass();
-        $root = __DIR__ . DIRECTORY_SEPARATOR . 'tmp' . DIRECTORY_SEPARATOR;
-        unlink($root . 'test.php');
-        unlink($root . 'subDir' . DIRECTORY_SEPARATOR . 'test.php');
-        unlink($root . 'testOutput.php');
-
-        rmdir($root . 'subDir');
-        rmdir($root);
-    }
-
     /**
      * @dataProvider providerDataLoadertInterface
      * @param $key
@@ -62,7 +21,7 @@ php;
      */
     protected function createLoaderInterfaceObject()
     {
-        return new IoC(__DIR__ . DIRECTORY_SEPARATOR . 'tmp');
+        return new IoC(__DIR__ . DIRECTORY_SEPARATOR . 'IocTestExample');
     }
 
     public function providerDataLoadertInterface()
@@ -105,7 +64,7 @@ php;
 
     public function testHas()
     {
-        $loader = new IoC(__DIR__ . DIRECTORY_SEPARATOR . 'tmp');
+        $loader = new IoC(__DIR__ . DIRECTORY_SEPARATOR . 'IocTestExample');
         $this->assertSame(true, $loader->has('test'));
         $this->assertSame(false, $loader->has('invalidName'));
         $this->assertSame(true, $loader->has('subDir_test'));
@@ -116,13 +75,13 @@ php;
 
     public function testhasNotString()
     {
-        $loader = new IoC(__DIR__ . DIRECTORY_SEPARATOR . 'tmp');
+        $loader = new IoC(__DIR__ . DIRECTORY_SEPARATOR . 'IocTestExample');
         assertFalse($loader->has(1));
     }
 
     public function testGet()
     {
-        $loader = new IoC(__DIR__ . DIRECTORY_SEPARATOR . 'tmp', ['a' => 5]);
+        $loader = new IoC(__DIR__ . DIRECTORY_SEPARATOR . 'IocTestExample', ['a' => 5]);
         // Загрузка простого компонента
         $this->assertSame(15, $loader->get('test'));
         $this->assertSame(15, $loader->get('subDir_test'));
@@ -134,7 +93,7 @@ php;
      */
     public function testLoadInvalidContainer()
     {
-        $loader = new IoC(__DIR__ . DIRECTORY_SEPARATOR . 'tmp');
+        $loader = new IoC(__DIR__ . DIRECTORY_SEPARATOR . 'IocTestExample');
         $loader->get('invalid');
     }
 
@@ -144,7 +103,7 @@ php;
      */
     public function testLoadNotString()
     {
-        $loader = new IoC(__DIR__ . DIRECTORY_SEPARATOR . 'tmp');
+        $loader = new IoC(__DIR__ . DIRECTORY_SEPARATOR . 'IocTestExample');
         $loader->get(1);
     }
 
@@ -153,7 +112,7 @@ php;
      */
     public function testLoadWithOutput()
     {
-        $loader = new IoC(__DIR__ . DIRECTORY_SEPARATOR . 'tmp');
+        $loader = new IoC(__DIR__ . DIRECTORY_SEPARATOR . 'IocTestExample');
         $loader->get('testOutput');
     }
 }
