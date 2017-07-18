d7e9e11b8acb854f7a13dbce191b7f326a883669
Evgeniy Kuvshinov <smpl@itmywork.com>
2017-07-19 01:21:41 +0500

    fixed #98

--- src/Provider/ReflectionTrait.php
+++ src/Provider/ReflectionTrait.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 namespace Smpl\Mydi\Provider;
 
-use Smpl\Mydi\Exception\NotFoundException;
-
 trait ReflectionTrait
 {
     private static $reflections = [];
@@ -16,45 +14,47 @@ trait ReflectionTrait
 
     public function has(string $name): bool
     {
-        $result = false;
-        try {
-            $class = static::getReflection($name);
-            $docComment = $class->getDocComment() !== false ? $class->getDocComment() : '';
-            if (strpos($docComment, '@' . $this->annotation) !== false
-                || empty($this->annotation)
-            ) {
-                $result = true;
-            }
-        } catch (NotFoundException $e) {
-            // Ошибку обрабатывать не стоит, понятно что ответ будет false
-            $result = false;
-        }
+        $docComment = $this->getDocComment($name);
+        $result = $this->isValidDocComment($docComment);
         return $result;
     }
 
     /**
      * @param string $id
-     * @throws NotFoundException В случае если id не может быть объектом reflection
      * @return \ReflectionClass
      */
     protected static function getReflection(string $id): \ReflectionClass
     {
-        if (!self::isReflection($id)) {
-            throw new NotFoundException();
-        }
         if (!array_key_exists($id, self::$reflections)) {
             self::$reflections[$id] = new \ReflectionClass($id);
         }
         return self::$reflections[$id];
     }
 
-    private static function isReflection(string $id): bool
-    {
-        return class_exists($id) || interface_exists($id);
-    }
-
     protected function setAnnotation(string $annotation)
     {
         $this->annotation = $annotation;
+    }
+
+    private function isValidDocComment(string $docComment): bool
+    {
+        $result = false;
+        if (strpos($docComment, '@' . $this->annotation) !== false
+            || empty($this->annotation)
+        ) {
+            $result = true;
+        }
+        return $result;
+    }
+
+    private function getDocComment(string $name): string
+    {
+        try {
+            $class = static::getReflection($name);
+            $docComment = $class->getDocComment() !== false ? $class->getDocComment() : '';
+        } catch (\Exception $e) {
+            $docComment = '';
+        }
+        return $docComment;
     }
 }
