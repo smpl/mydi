296917adca80bde99368548b52809a91da601a8b
Evgeniy Kuvshinov <smpl@itmywork.com>
2018-07-31 03:12:42 +0500

    Service больше не extend Factory

--- src/Loader/Service.php
+++ src/Loader/Service.php
@@ -3,17 +3,25 @@ declare(strict_types=1);
 
 namespace Smpl\Mydi\Loader;
 
+use Closure;
 use Psr\Container\ContainerInterface;
 
-class Service extends Factory
+class Service
 {
     private $result;
     private $isCalled = false;
+    private $closure;
+
+    public function __construct(Closure $closure)
+    {
+        $this->closure = $closure;
+    }
+
 
     public function load(ContainerInterface $container)
     {
         if (!$this->isCalled) {
-            $this->result = parent::load($container);
+            $this->result = call_user_func_array($this->closure, [$container]);
             $this->isCalled = true;
         }
         return $this->result;
